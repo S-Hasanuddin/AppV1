ed75622d7685c317e19f4ec25bd4240254404c1c
S-Hasanuddin <s.hasanuddin20@gmail.com>
2024-12-13 19:37:27 +0530

    commit 4- updated backend + front, need to work on frontend

--- app/src/main/java/com/example/myapplication/ui/login/LoginActivity.kt
+++ app/src/main/java/com/example/myapplication/ui/login/LoginActivity.kt
@@ -1,10 +1,11 @@
-package com.example.myapplication
+package com.example.myapplication.ui.login
 
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.example.myapplication.MainActivity
 import com.example.myapplication.databinding.ActivityLoginBinding
 import com.google.firebase.auth.FirebaseAuth
 
